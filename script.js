12577bdd2588e856eb5cc9b9976ad000c0a756ff
Felipe <nascimentofelipe565@gmail.com>
2021-02-24 00:26:28 -0300

    'New Transaction' working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Olá, as setas (↓ e ↑) mostram qual parte do código que estou comentando :p
 
-// Constante que abre e fecha o modal ↓ 
+// Constante que abre e fecha o modal. ↓ 
 const Modal = {
     open() {
         document
@@ -14,28 +14,7 @@ const Modal = {
             .classList
             .remove('active');
     }
-}
-
-// Constante que guarda as transações ↓
-const transactions = [{
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021'
-    },
-    {
-        id: 2,
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021'
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021'
-    }
-]
+};
 
 //  Constante que guarda as funções de contagem
 // na tabela e na parte de balanço. ↓
@@ -43,12 +22,37 @@ const BalanceValues = {
 
     //  Propriedade da constante que tem
     // todas as transações. ↓
-    all: transactions,
+    all: [{
+            description: 'Luz',
+            amount: -50000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Website',
+            amount: 500000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Internet',
+            amount: -20000,
+            date: '23/01/2021'
+        }
+    ],
 
     //  Função que adiciona uma nova transação ao
     // array de transações. ↓
     add(transaction) {
         BalanceValues.all.push(transaction);
+
+        App.reload();
+    },
+
+    //  Função que remove uma transação do array
+    // de transações. ↓
+    remove(index) {
+        BalanceValues.all.splice(index, 1);
+
+        App.reload();
     },
 
     // Função que calcula as entradas. ↓
@@ -80,10 +84,10 @@ const BalanceValues = {
         // entradas - saídas
         return BalanceValues.incomes() + BalanceValues.expenses();
     }
-}
+};
 
 // Aqui ficam todos as funções que trabalham mudando a página
-// usando o DOM ↓
+// usando o DOM. ↓
 const DOM = {
     // Seleção da tabela usando o DOM ↓
     transactionsContainer: document.querySelector('#data-table tbody'),
@@ -131,10 +135,12 @@ const DOM = {
     clearTransactions() {
         DOM.transactionsContainer.innerHTML = '';
     }
-}
+};
 
+//  Constante que guarda a função de colocar a 
+// formatação da moeda. ↓
 const Util = {
-    // Formatação do valor da transação
+    // Formatação do valor da transação. ↓
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : "";
 
@@ -146,11 +152,105 @@ const Util = {
         })
 
         return signal + value;
+    },
+
+    //  Função que formata o valor inserido, tirando as
+    // vírgulas e os pontos. ↓
+    formatAmount(value) {
+        return value = Number(value) * 100;
+    },
+
+    // Função que formata a data. ↓
+    formatDate(date) {
+        const splittedDate = date.split("-");
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
     }
 }
 
+const Form = {
+
+    // Propriedades que vem junto do formulário. ↓
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
+
+    //  Função que retorna o valor dos inputs
+    // do DOM. ↓
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value,
+        }
+    },
+
+    // Função que valida os campos do Form. ↓
+    validateFields() {
+        const {
+            description,
+            amount,
+            date
+        } = Form.getValues();
+
+        if (description.trim() === "" || amount.trim() === "" || date.trim() === "") { // verifica se os campos estão vazios.
+            throw new Error("Por favor, preencha todos os campos.");
+        }
+
+    },
+
+    // Função que formata os dados. ↓
+    formatValues() {
+        let {
+            description,
+            amount,
+            date
+        } = Form.getValues();
+
+        amount = Util.formatAmount(amount);
+        date = Util.formatDate(date);
+
+        console.log(description, amount, date);
+        return {
+            description,
+            amount,
+            date
+        }
+    },
+    clearFields() {
+        Form.description.value = '';
+        Form.amount.value = '';
+        Form.date.value = '';
+    },
+
+    //  Função que cuida das informações do
+    // formulário e coloca no array. ↓
+    submit(event) {
+        event.preventDefault();
+
+        try {
+            // Os campos são validados (ver se não estão vazios). ↓
+            Form.validateFields();
+
+            // Os dados são formatados pra D/M/A. ↓
+            const transaction = Form.formatValues();
+
+            // Os dados são salvos. ↓
+            BalanceValues.add(transaction);
+
+            // Os campos são limpos. ↓
+            Form.clearFields();
+
+            // Por fim, modal é fechado. ↓
+            Modal.close();
+
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+};
+
 //  Constante com as 'maiores' funções da página,
-// como carregar as funções iniciais. ↓ 
+// como carregar as funções iniciais. ↓
 const App = {
 
     //  Aqui ele cria cada um dos <tr> na página e 
@@ -168,4 +268,7 @@ const App = {
         DOM.clearTransactions();
         App.init();
     }
-}
+};
+
+// Função é chamada pra iniciar a aplicação. ↓
+App.init();
